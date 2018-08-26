97f6a0818bdc692361c3c099afb85b97a4427519
decagondev <decagondevelopment@gmail.com>
2018-08-26 23:42:29 +0100

    (project_manager) installed npm package uuid as a dependency

--- project_manager/src/Components/AddProject.js
+++ project_manager/src/Components/AddProject.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import uuid from 'uuid';
 
 class AddProject extends Component {
     constructor() {
@@ -17,6 +18,7 @@ class AddProject extends Component {
             alert('title is required');
         } else {
             this.setState({newProject: {
+                id: uuid.v4(),
                 title: this.refs.title.value,
                 category: this.refs.category.value
             }}, function() {
@@ -29,7 +31,7 @@ class AddProject extends Component {
   render() {
 
     let categoryOptions = this.props.categories.map(category => {
-        return <option key={category} value={category}>{category}</option>
+        return <option key={category.id} value={category}>{category}</option>
     });
 
     return (
